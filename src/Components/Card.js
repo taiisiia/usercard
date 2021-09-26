fc1b7ddfa15605bbe72c31c512f16fbb5ac34a76
Taiisiia <melnikovataya@gmail.com>
2021-09-26 15:00:47 +0300

    initial commit

---
+++ src/Components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Card.css'
+
+function Card( {
+    return (
+        <div className = 'Card'>
+            <div className ='upper-area'>
+                <div className = 'image-wrapper'>
+                <img src = '' alt = "" height = "100px" width = "100px" />
+                </div>
+            </div>
+            <div className = "lower-area">
+                
+            </div>
+        </div>
+    )
+})
+
+export default Card
