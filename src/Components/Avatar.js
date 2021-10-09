23d0e8fde5274aa8a764c5c1dbb08dd366416efc
Taiisiia <melnikovataya@gmail.com>
2021-10-09 17:03:33 +0300

    add avatar

--- src/Components/Avatar.js
+++ src/Components/Avatar.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import avatar from '../assets/images/avatar.png';
 
 function Avatar () {
     return(
-    <img className = "pic" src = 'https://lh3.googleusercontent.com/proxy/bEuckZU_sAfB96gMmFohHjJT9NvvG97jTQlmXF_yaiG4b-MDdPceCZpQSt3ee1k-2m1mLbiifHlH1CwMT4yGotMV52wZyzL_pBjaPTuo0quuTtXj' alt = "profile pic"  />
+    <img className = "pic" src = {avatar} alt = "profile pic"  />
     )}
 
 export default Avatar 
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+import React from 'react'
 import './Card.css'
 import Avatar from './Avatar'
 
